7d7526f0b25b3a81a33d3c5912631b2fa18b09fc
Alex Ott <alexott@gmail.com>
2010-07-22 16:05:30 +0200

    use clojure 1.2.0 in example

--- hadoop1/project.clj
+++ hadoop1/project.clj
@@ -1,6 +1,6 @@
 (defproject hadoop1 "1.0-SNAPSHOT"
   :description "hadoop1"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [com.stuartsierra/clojure-hadoop "1.2.0-SNAPSHOT"]]
   )

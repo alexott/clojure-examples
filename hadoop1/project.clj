e630c828a654b0670a3a20a61a6072c5d2e75e94
Alex Ott <alexott@gmail.com>
2010-08-29 13:21:54 +0200

    add lein-hadoop as dev dependency

--- hadoop1/project.clj
+++ hadoop1/project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.stuartsierra/clojure-hadoop "1.2.0"]]
+  :dev-dependencies [[lein-hadoop "1.0.0"]]
   )
